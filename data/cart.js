4a0ce4d7f29b3c504f5b11501115805dbf4c29f4
KoustavDev <koustav.mj.official@gmail.com>
2024-07-07 01:16:06 +0530

    removeFromCart function is optimized.

--- data/cart.js
+++ data/cart.js
@@ -54,12 +54,6 @@ export function addToCart(productId) {
 }
 
 export function removeFromCart(productId) {
-  const newCart = [];
-  cart.forEach((cartItem) => {
-    if (cartItem.productId !== productId) {
-      newCart.push(cartItem);
-    }
-  });
-  cart = newCart;
+  cart = cart.filter((cartItem)=>{cartItem.productId !== productId});
   saveToStorage();
 }
